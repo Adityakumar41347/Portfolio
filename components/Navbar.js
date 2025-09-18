fb88732675b971f5afa99f02cc1841d71ba7b500
Aditya Thakur <adityakumar41347@gmail.com>
2025-09-18 15:10:28 +0530

    Working on portfolio with next js

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-end  text-black mx-auto container mt-3'>
+      <div className='p-4 font-semibold text-2xl '>Work</div>
+      <div className='p-4 font-semibold text-2xl'>Blog</div>
+      <div className='p-4 font-semibold text-2xl'>Contact</div>
+    </div>
+  )
+}
+
+export default Navbar
