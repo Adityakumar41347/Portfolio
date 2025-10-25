533f9c8c6257940e0f7b20893e9c13d2db11bbec
Aditya Thakur <adityakumar41347@gmail.com>
2025-10-25 17:54:32 +0530

    correcting the routing

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,10 +13,10 @@ const Navbar = () => {
     <div className={`flex md:pr-40 justify-end  text-black mx-auto w-full  pt-3 ${mode?"bg-zinc-900 text-gray-300": "bg-white"}`}> 
       <button onClick={() => setMode((mode + 1) % 2)}><img className={`w-10 border-2  p-2 rounded-full ${mode?"bg-gray-300  invert-100":"bg-gray-200 border-white text-white"}`} src="/dark.svg" alt="" /></button>
       
-      <Link href={`${process.env.NEXT_PUBLIC_HOST}`}><div className={`{} md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/" ? 'text-red-600 hover: border-b-3 border-red-600 ':'hover:underline'}`}>Home</div></Link>
-      <Link href={`${process.env.NEXT_PUBLIC_HOST}/work`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/work" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Work</div></Link>
-      <Link href={`${process.env.NEXT_PUBLIC_HOST}/skill`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/skill" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Toolset</div></Link>
-      <Link href={`${process.env.NEXT_PUBLIC_HOST}/contact`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/contact" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Contact</div></Link>
+      <Link href={`/`}><div className={`{} md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/" ? 'text-red-600 hover: border-b-3 border-red-600 ':'hover:underline'}`}>Home</div></Link>
+      <Link href={`/work`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/work" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Work</div></Link>
+      <Link href={`/skill`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/skill" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Toolset</div></Link>
+      <Link href={`/contact`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/contact" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Contact</div></Link>
     </div>
   )
 }
