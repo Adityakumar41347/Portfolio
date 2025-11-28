3323167dcca0bd1f7f92cf79b2cd739ad8b3e8d7
Aditya Thakur <adityakumar41347@gmail.com>
2025-11-28 21:59:34 +0530

    and all set

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = () => {
       <button onClick={() => setMode((mode + 1) % 2)}><img className={`w-10 border-2  p-2 rounded-full ${mode?"bg-gray-300  invert-100":"bg-gray-200 border-white text-white"}`} src="/dark.svg" alt="" /></button>
       
       <Link href={`/`}><div className={`{} md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/" ? 'text-red-600 hover: border-b-3 border-red-600 ':'hover:underline'}`}>Home</div></Link>
-      <Link href={`/work`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/work" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Work</div></Link>
-      <Link href={`/skill`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/skill" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Toolset</div></Link>
-      <Link href={`/contact`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/contact" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Contact</div></Link>
+      <Link href={`/work`}><div className={`md:m-4 my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=='/work' || path=='/work/linktree' || path=='/work/patreon' ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Work</div></Link>
+      <Link href={`/skill`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/skill" || path=="/web-dev" || path=="/dsa" ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Toolset</div></Link>
+      <Link href={`/contact`}><div className={`md:m-4  my-4 mx-2 p-2 cursor-pointer font-semibold text-xl ${path=="/contact"  ? 'text-red-600 border-b-3 border-red-600':'hover:underline'}`}>Contact</div></Link>
     </div>
   )
 }
